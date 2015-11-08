76e666f269db99565a1d871b63b3efaade89842f
Pierre Beaucamp <mail@pierrebeaucamp.com>
2015-11-08 11:25:57 -0500

    deleted main query on index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
                 <button type="button" id="btn_activities" class="btn btn-primary btn-lg">At home Activities</button>
                 <ul id="activity_menu" style="list-style: none">
                     <?php wp_list_categories(array(
-                        'show_option_all'    => 'all',
+                        'show_option_all'    => '',
                         'orderby'            => 'name',
                         'order'              => 'ASC',
                         'style'              => 'list',
@@ -33,6 +33,9 @@
                         'taxonomy'           => 'category',
                         'walker'             => null
                     )); ?>
+                    <li class="cat-item-all">
+                        <a href="/">all</a>
+                    </li>
                 </ul>
             </div>
 
@@ -50,46 +53,5 @@
         </div>
     </div>
 </div>
-
-<main class="container" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
-            <header class="article-header">
-                <h1 class="h2 entry-title"><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-                <p class="byline entry-meta vcard">
-                    <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
-                    /* the time the post was published */
-                    '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-                    /* the author of the post */
-                    '<span class="by">'.__( 'by', 'bonestheme').'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-                    ); ?>
-                </p>
-            </header>
-
-            <section class="entry-content cf">
-                <?php the_content(); ?>
-            </section>
-
-            <footer class="article-footer cf">
-                <?php printf( '<p class="footer-category">' . __('filed under', 'bonestheme' ) . ': %1$s</p>' , get_the_category_list(', ') ); ?>
-                <?php the_tags( '<p class="footer-tags tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-            </footer>
-        </article>
-    <?php endwhile; ?>
-    <?php bones_page_navi(); ?>
-    <?php else : ?>
-        <article id="post-not-found" class="hentry cf">
-            <header class="article-header">
-                <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-            </header>
-            <section class="entry-content">
-                <p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-            </section>
-            <footer class="article-footer">
-                <p><?php _e( 'This is the error message in the index.php template.', 'bonestheme' ); ?></p>
-            </footer>
-        </article>
-    <?php endif; ?>
-</main>
 
 <?php get_footer(); ?>
