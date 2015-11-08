c4c2ba66f566272071cb5521eebea1053788282b
Pierre Beaucamp <mail@pierrebeaucamp.com>
2015-11-08 13:00:54 -0500

    styled contegory and search page

--- archive.php
+++ archive.php
@@ -8,8 +8,8 @@
     <br>
 
     <div class="row">
-        <div class="result-item">
-            <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+            <div class="result-item">
                 <article id="post-<?php the_ID(); ?>" <?php post_class( 'result-item-text' ); ?> role="article">
                     <header>
                         <a class="post-item-text-name" href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
@@ -19,19 +19,21 @@
                         <?php the_excerpt(); ?>
                     </section>
                 </article>
-            <?php endwhile; ?>
-            <?php bones_page_navi(); ?>
-            <?php else : ?>
+            </div>
+        <?php endwhile; ?>
+        <?php bones_page_navi(); ?>
+        <?php else : ?>
+            <div class="result-item">
                 <article id="post-not-found" class="hentry cf">
                     <header class="article-header">
-                        <h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+                        <p><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></p>
                     </header>
                     <section class="entry-content">
                         <p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
                     </section>
                 </article>
-            <?php endif; ?>
-        </div>
+            </div>
+        <?php endif; ?>
     </div>
 </main>
 <?php get_footer(); ?>
